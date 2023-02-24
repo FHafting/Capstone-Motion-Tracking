f8aa09d801ff707c67052020fff64d760694dd15
shivuwo <116472257+shivuwo@users.noreply.github.com>
2023-02-24 12:38:49 -0500

    added esp now functionality (still missing json string construction)

--- ESP32_Wifi/include/headers.h
+++ ESP32_Wifi/include/headers.h
@@ -16,3 +16,4 @@
 #include <Wire.h>
 //#include <Adafruit_Sensor.h>
 //#include <Adafruit_BME280.h>
+#include <esp_now.h>
--- ESP32_Wifi/src/esp32_mpu_plotter.ino
+++ ESP32_Wifi/src/esp32_mpu_plotter.ino
@@ -1,143 +1,119 @@
 #include "headers.h"
 // #include "functions.h"
 
-// Replace with your network credentials
-const char *ssidSTA = "CANADIAN WINTER BELL";
-const char *passwordSTA = "Shivam1912__";
-
-const char *ssidSTA2 = "iPhone (100)";
-const char *passwordSTA2 = "shiv1912";
-
 const char *ssidAP = "ESP32_Shiv";
 const char *passwordAP = "12345678";
-
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-const int MPU_ADDR = 0x68;            // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
-int accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int gyro_x, gyro_y, gyro_z;           // variables for gyro raw data
-int temperature;                      // variables for temperature data
+const int packetSize = 10; // amount of sensor readings being sent within each packet
+// Must match the sender structure
 
-void mpuSetup()
+// initializing esp now
+typedef struct struct_message
 {
-  Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-  Wire.write(0x6B);                 // PWR_MGMT_1 register
-  Wire.write(0);                    // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+  int id;
+  String time;
+  int accelx[packetSize];
+  int accely[packetSize];
+  int accelz[packetSize];
+  int gyrox[packetSize];
+  int gyroy[packetSize];
+  int gyroz[packetSize];
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Creating upto 6 esp 32 devices
+struct_message board1;
+struct_message board2;
+struct_message board3;
+struct_message board4;
+struct_message board5;
+struct_message board6;
+
+// Create an array with all the structures
+struct_message boardsStruct[6] = {board1, board2, board3, board4, board5, board6};
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
+{
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id - 1].time = myData.time;
+  Serial.printf("time value: %d \n", boardsStruct[myData.id - 1].time);
+  for (int i = 0; i++; i < packetSize)
+  {
+    boardsStruct[myData.id - 1].accelx[i] = myData.accelx[i];
+    Serial.printf("accelx value%d: %d \n", i, boardsStruct[myData.id - 1].accelx[i]);
+    boardsStruct[myData.id - 1].accely[i] = myData.accely[i];
+    boardsStruct[myData.id - 1].accelz[i] = myData.accelz[i];
+    boardsStruct[myData.id - 1].gyrox[i] = myData.gyrox[i];
+    boardsStruct[myData.id - 1].gyroy[i] = myData.gyroy[i];
+    boardsStruct[myData.id - 1].gyroz[i] = myData.gyroz[i];
+  }
+
+  Serial.println();
 }
 
-String mpuRead(int index)
+void espNowSetup()
 {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);                        // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false);             // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // request a total of 7*2=14 registers
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
-  float result[7];
-  result[0] = Wire.read() << 8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  result[1] = Wire.read() << 8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  result[2] = Wire.read() << 8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  result[3] = Wire.read() << 8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  result[4] = Wire.read() << 8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  result[5] = Wire.read() << 8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  result[6] = Wire.read() << 8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
-
-  float result2;
-  switch (index)
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
   {
-  case 0:
-    result2 = result[0];
-    break;
-  case 1:
-    result2 = result[1];
-    break;
-  case 2:
-    result2 = result[2];
-    break;
-  case 3:
-    result2 = result[3];
-    break;
-  case 4:
-    result2 = result[4];
-    break;
-  case 5:
-    result2 = result[5];
-    break;
-  case 6:
-    result2 = result[6];
-    break;
-  default:
-    result2 = 0;
-    break;
-  }
-
-  if (isnan(result2))
-  {
-    Serial.println("Failed to read MPU6050 Value!");
-    return "";
-  }
-  else
-  {
-    Serial.println(result2);
-    return String(result2);
-  }
-}
-
-void spiffsSetup()
-{
-  // Initialize SPIFFS
-  SPIFFS.begin(true);
-  if (!SPIFFS.begin())
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
+    Serial.println("Error initializing ESP-NOW");
     return;
   }
+
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
+// wifi setup for wifi transmission
 void wifiSetup()
 {
   // Connect to Wi-Fi
-  int connectionMode = 1;
+  WiFi.softAP(ssidAP, passwordAP);
+  Serial.println(WiFi.softAPIP());
+}
 
-  if (connectionMode == 1)
-  {
-    WiFi.softAP(ssidAP, passwordAP);
-    Serial.println(WiFi.softAPIP());
-  }
-  else
-  {
+// establishing functions for data transmission
+String testData1()
+{
+  String test = "Hey this is test set #1";
+  return test;
+}
 
-    WiFi.begin(ssidSTA, passwordSTA);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(100);
-      Serial.println("Connecting to WiFi..");
-    }
-    // Print ESP32 Local IP Address
-    Serial.println(WiFi.localIP());
-  }
-
-  
+String testData2()
+{
+  String test = "{\"ESP32 ID\":1,\n\"Sensor ID\":1,\n\"Time\":\"10:0000\",\n\"Value\": 12453.6987}";
+  return test;
 }
 
 void setup()
 {
   // Serial port for debugging purposes
   Serial.begin(115200);
-  mpuSetup();
-  spiffsSetup();
+
   wifiSetup();
+  espNowSetup();
+
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/index.html"); });
-  server.on("/accelx", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/plain", mpuRead(0).c_str()); });
-  server.on("/accely", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/plain", mpuRead(1).c_str()); });
-  server.on("/accelz", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/plain", mpuRead(2).c_str()); });
+            { request->send_P(200, "text/plain", testData1().c_str()); });
+  server.on("/test2", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/plain", testData2().c_str()); });
 
   // Start server
   server.begin();
@@ -146,5 +122,11 @@ void setup()
 void loop()
 {
 
-  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  // Acess the variables for each board
+  /*int board1X = boardsStruct[0].x;
+  int board1Y = boardsStruct[0].y;
+  int board2X = boardsStruct[1].x;
+  int board2Y = boardsStruct[1].y;
+  int board3X = boardsStruct[2].x;
+  int board3Y = boardsStruct[2].y;*/
 }
